fe65d61eb75995d780c4f0e288d42109c203e9e4
sebastian baiju <41418122+SebastianBaiju@users.noreply.github.com>
2022-02-04 16:26:29 +0530

    Delete api directory

--- api/apitest/serializer.py
+++
@@ -1,9 +0,0 @@
-import logging
-from django.conf import settings
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from apitest.models import Employees
-logger = logging.getLogger(__name__)
-class EmployeeSeriallizer(ModelSerializer):
-    class Meta:
-          model = Employees
-          fields = ['name']
